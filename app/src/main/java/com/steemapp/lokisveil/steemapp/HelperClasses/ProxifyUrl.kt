f6f8ca348a7ef4eb1c12413dce4372a8a275794f
hispeedimagins <36503820+hispeedimagins@users.noreply.github.com>
2018-06-16 18:02:09 +0530

    Remarkable article rendered and steemit linkify
    Added Remarkable js to render like steemit. Translated steemit linkify, image renderers and phishy links to kotlin. Implemented hash tag catcher and proxy images

---
+++ app/src/main/java/com/steemapp/lokisveil/steemapp/HelperClasses/ProxifyUrl.kt
@@ -0,0 +1,39 @@
+package com.steemapp.lokisveil.steemapp.HelperClasses
+
+import java.net.URLEncoder
+
+//implementation of  proxify class from steemit
+class ProxifyUrl {
+    companion object {
+        val rProxyDomain = "^http(s)?://steemit(dev|stage)?images.com/"
+        val rProxyDomainsDimensions = "http(s)?://steemit(dev|stage)?images.com/([0-9]+x[0-9]+)/"
+        val NATURAL_SIZE = "0x0/"
+        val img_proxy_prefix = "https://steemitdevimages.com/"
+
+
+        //returns the url which will be rendered
+        //by the dimension or zero
+        fun proxyurl(url:String,dimensions:Any = false) : String{
+            var reg = rProxyDomainsDimensions.toRegex()
+            var regm = reg.findAll(url)
+            var respUrl = url
+            if(regm.any()){
+                var lastproxy = regm.last()
+                respUrl = url.substring(url.lastIndexOf(lastproxy.value) + lastproxy.value.length)
+            }
+            if(!img_proxy_prefix.isNullOrEmpty()){
+                var dims = ""
+                if(dimensions is String)
+                    dims = "$dimensions/"
+                else if(dimensions is Boolean)
+                    dims = if(regm.any()) "([0-9]+x[0-9]+)/".toRegex().find(regm.first().value)?.groupValues?.first()!! else NATURAL_SIZE
+
+                if (NATURAL_SIZE !== dims || !rProxyDomain.toRegex().matches(respUrl)) {
+                    return img_proxy_prefix + dims + URLEncoder.encode(respUrl, "utf-8")
+                }
+
+            }
+            return respUrl
+        }
+    }
+}
