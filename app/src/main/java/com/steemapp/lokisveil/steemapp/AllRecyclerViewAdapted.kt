c41fe9f56b2e9f1befa8adcf7fd301dbc278f2df
hispeedimagins <36503820+hispeedimagins@users.noreply.github.com>
2018-12-16 16:35:38 +0530

    code cleanup

--- app/src/main/java/com/steemapp/lokisveil/steemapp/AllRecyclerViewAdapted.kt
+++ app/src/main/java/com/steemapp/lokisveil/steemapp/AllRecyclerViewAdapted.kt
@@ -1,10 +1,13 @@
 package com.steemapp.lokisveil.steemapp
 
 import android.app.Activity
+import android.arch.paging.PagedList
+import android.arch.paging.PagedListAdapter
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Resources
 import android.os.AsyncTask
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
@@ -14,7 +17,7 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import com.google.android.gms.ads.AdRequest
+//import com.google.android.gms.ads.AdRequest
 import com.google.gson.Gson
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
 import com.steemapp.lokisveil.steemapp.BindHelpers.*
@@ -32,12 +35,15 @@ import com.steemapp.lokisveil.steemapp.jsonclasses.feed
 import com.steemapp.lokisveil.steemapp.jsonclasses.prof
 import java.util.*
 import kotlin.collections.ArrayList
+import android.arch.paging.AsyncPagedListDiffer
+
+
 
 /**
  * Created by boot on 2/4/2018.
  */
 
-public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>, thisRecyclerView: RecyclerView, view: View?, initiate: AdapterToUseFor,globalInterface: GlobalInterface? = null) : RecyclerView.Adapter<RecyclerView.ViewHolder>() , arvdinterface , FastScrollRecyclerView.SectionedAdapter {
+class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>, thisRecyclerView: RecyclerView, view: View?, initiate: AdapterToUseFor,globalInterface: GlobalInterface? = null) : RecyclerView.Adapter<RecyclerView.ViewHolder>() , arvdinterface , FastScrollRecyclerView.SectionedAdapter {
     override fun getSectionName(position: Int): String {
         var ob = mValues?.get(position)
         when(apptype){
@@ -57,6 +63,52 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
         else if(f)*/
 
 
+    }
+
+    public class ArticlePaged():PagedListAdapter<FeedArticleDataHolder.FeedArticleHolder, ArticleViewHolder>(AllRecyclerViewAdapter.article_DIFF_CALLBACK){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+            return ArticleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.article_preview, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+
+        }
+
+        override fun getCurrentList(): PagedList<FeedArticleDataHolder.FeedArticleHolder>? {
+            return super.getCurrentList()
+        }
+
+        override fun getItem(position: Int): FeedArticleDataHolder.FeedArticleHolder? {
+            return super.getItem(position)
+        }
+
+        override fun getItemCount(): Int {
+            return super.getItemCount()
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return super.getItemViewType(position)
+        }
+
+        override fun submitList(pagedList: PagedList<FeedArticleDataHolder.FeedArticleHolder>?) {
+            super.submitList(pagedList)
+        }
+    }
+
+
+    companion object {
+        val  article_DIFF_CALLBACK = object :
+                DiffUtil.ItemCallback<FeedArticleDataHolder.FeedArticleHolder>() {
+            // Concert details may have changed if reloaded from the database,
+            // but ID is fixed.
+            override fun areItemsTheSame(oldConcert: FeedArticleDataHolder.FeedArticleHolder,
+                                         newConcert: FeedArticleDataHolder.FeedArticleHolder): Boolean =
+                    oldConcert.id == newConcert.id
+
+            override fun areContentsTheSame(oldConcert: FeedArticleDataHolder.FeedArticleHolder,
+                                            newConcert: FeedArticleDataHolder.FeedArticleHolder): Boolean =
+                    oldConcert == newConcert
+        }
     }
 
 
@@ -79,11 +131,12 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
         }
     }
 
+    private val mDiffer = AsyncPagedListDiffer(this, article_DIFF_CALLBACK)
     private var globalInterface = globalInterface
     private var mValues: MutableList<Any>? = null
     internal var activity = activity
     //private final questionslistFragment.OnListFragmentInteractionListener mListener;
-    //internal var calcs: calendarcalculations
+    //    //internal var calcs: calendarcalculations
     internal var metrics: DisplayMetrics? = null
     internal var context: Activity? = null
     internal var gson: Gson? = null
@@ -104,26 +157,10 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
     private val isHeader = 900
     private val isSearchUser = 1000
     private val isDate = 1100
-    /*private val isdateview = 65
-    private val ischatview = 72
-    private val isadview = 74
-    private val isQuestionView = 122
-    private val isPeopleView = 125
-    private val isNoMessageView = 161
-    private val isOpenAQuestionView = 187
-    private val isNotificationView = 9877
-    private val isTenorView = 1001*/
     internal var datetopaste: Calendar? = null
     private val pastethedatespecifically: Boolean = false
     private val messagecounter: Long = 0
-    //private val fontSingleton: FontSingleton
-    //private var floatingDateHolder: FloatingDateHolder? = null
     private val ownInstance: AllRecyclerViewAdapter? = null
-    //var chatMessageFunctions: ChatMessageFunctions
-
-
-    //private Activity activity;
-    //private MyQuestionsMainListDb db;
     var notificationsBusyHelperFunctions : NotificationsBusyHelperFundtions? = null
     var feedHelperFunctions: FeedHelperFunctions? = null
     var blogHelperFunctions: FeedHelperFunctions? = null
@@ -137,26 +174,15 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
     var headerHelperFunctions : HeaderHelperFunctions? = null
     var searchUsersHelperFunctions:SearchUsersHelperFunctions? = null
     var floatingDateHolder : FloatingDateHolder? = null
-    //var otherguy = otherguy
-    /*var peopleFunctionsList: PeopleFunctionsList
-    var openAQuestionHelperFunctions: OpenAQuestionHelperFunctions
-
-    var notificationListFunctions: NotificationListFunctions
-
-    var tenorHelperFunctions: TenorHelperFunctions*/
-
-    private val adRequest: com.google.android.gms.ads.AdRequest? = null
+    //private val adRequest: com.google.android.gms.ads.AdRequest? = null
     internal var adapterInterface: AllRecyclerViewAdapterInterface? = null
     var apptype = initiate
 
     init{
         mValues = items
-        //mListener = listener;
         if (activity is AllRecyclerViewAdapterInterface) {
-            this.adapterInterface = activity as AllRecyclerViewAdapterInterface
+            this.adapterInterface = activity
         }
-
-        //this.calcs = calendarcalculations()
 
         this.recyclerView = thisRecyclerView
         this.prevdate = Calendar.getInstance()
@@ -165,44 +191,29 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
         this.datetopaste = Calendar.getInstance()
         this.gson = Gson()
         this.context = activity
-        //this.fontSingleton = FontSingleton.getInstance(activity)
         metrics = DisplayMetrics()
         activity.windowManager.defaultDisplay.getMetrics(metrics)
         this.s = context?.getSharedPreferences(CentralConstants.sharedprefname, 0)
         this.appUserName = s?.getString("username", null)
-
         scale = context?.resources?.displayMetrics?.density
-       // this.adRequest = AdRequest.Builder().build()
-
-
         this.registerAdapterDataObserver(observer)
         observer.onChanged()
-
-        //sometimes username is null, check it before going forward
         appUserName = if(appUserName != null) appUserName as String else context?.getSharedPreferences(CentralConstants.sharedprefname, 0)!!.getString("username", null)
-
         when (initiate) {
             AdapterToUseFor.feed -> {
-                //this.floatingDateHolder = FloatingDateHolder(context, view, recyclerView, this.fontSingleton, this@AllRecyclerViewAdapter)
-                //this.chatMessageFunctions = ChatMessageFunctions(metrics, context, floatingDateHolder, this@AllRecyclerViewAdapter)
                 this.floatingDateHolder = FloatingDateHolder(context!!,view!!,recyclerView!!,this)
                 this.feedHelperFunctions = FeedHelperFunctions(context as Context,appUserName!!,this,AdapterToUseFor.feed,this.floatingDateHolder)
             }
             AdapterToUseFor.blog -> {
                 this.floatingDateHolder = FloatingDateHolder(context!!, view!!, recyclerView!!, this@AllRecyclerViewAdapter)
-              //  this.questionListFunctions = QuestionListFunctions(context, this@AllRecyclerViewAdapter, floatingDateHolder)
-               // this.peopleFunctionsList = PeopleFunctionsList(mValues, this@AllRecyclerViewAdapter)
                 this.blogHelperFunctions = FeedHelperFunctions(context as Context,appUserName!!,this,AdapterToUseFor.blog,this.floatingDateHolder)
             }
 
             AdapterToUseFor.notifications -> {
                 this.floatingDateHolder = FloatingDateHolder(context!!,view!!,recyclerView!!,this)
                 this.notificationsBusyHelperFunctions = NotificationsBusyHelperFundtions(context as Context,appUserName!!,this,this.floatingDateHolder)
-                //this.peopleFunctionsList = PeopleFunctionsList(mValues, this@AllRecyclerViewAdapter)
             }
             AdapterToUseFor.wallet -> {
-              //  this.floatingDateHolder = FloatingDateHolder(context, view, recyclerView, this.fontSingleton, this@AllRecyclerViewAdapter)
-              //  this.questionListFunctions = QuestionListFunctions(context, this@AllRecyclerViewAdapter, floatingDateHolder)
             }
             AdapterToUseFor.article ->{
 
@@ -242,20 +253,6 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
                 this.searchUsersHelperFunctions = SearchUsersHelperFunctions(context as Context,appUserName as String ,this,AdapterToUseFor.followers)
                 this.headerHelperFunctions = HeaderHelperFunctions(context as Context,appUserName as String,this,apptype)
             }
-
-                //this.openAQuestionHelperFunctions = OpenAQuestionHelperFunctions(metrics, calcs, this.appUserName, this.fontSingleton, this@AllRecyclerViewAdapter)
-            /*AdapterToUseFor.feed -> {
-                this.floatingDateHolder = FloatingDateHolder(context, view, recyclerView, this.fontSingleton, this@AllRecyclerViewAdapter)
-                this.notificationListFunctions = NotificationListFunctions(context, this@AllRecyclerViewAdapter, this.floatingDateHolder)
-            }
-            AdapterToUseFor.feed -> {
-                //thisRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,1));
-                tenorHelperFunctions = TenorHelperFunctions(appUserName, this@AllRecyclerViewAdapter)
-                val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-                layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
-                //thisRecyclerView.setLayoutManager(new GridLayoutManager(context,2));
-                thisRecyclerView.layoutManager = layoutManager
-            }*/
             else -> {
             }
         }
@@ -265,9 +262,6 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        /*View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.chat_message_layout, parent, false);
-        return new ViewHolder(view);*/
         var v: View? = null
         var vh: RecyclerView.ViewHolder? = null
         if (viewType == isFeedView) {
@@ -318,23 +312,7 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
             v = LayoutInflater.from(parent.context).inflate(R.layout.dateresourceforchat, parent, false)
             vh = DateViewHolder(v)
         }
-        /*else if (viewType == ischatview) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.chat_message_layout, parent, false)
-            vh = ChatViewHolder(v)
-        }
-        if (viewType == isQuestionView) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.questionviewkeeper, parent, false)
-            vh = QuestionsListViewHolder(v)
-        } else if (viewType == isdateview) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.dateresourceforchat, parent, false)
-            vh = DateViewHolder(v)
-        } else if (viewType == isPeopleView) {
-
-            v = LayoutInflater.from(parent.context).inflate(R.layout.peopleviewkeeper, parent, false)
-            vh = PeopleViewHolder(v)
-        } else if (viewType == isNoMessageView) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.messageifnothingtoshow, parent, false)
-            vh = NothingToShow(v)
+        /*
         } else if (viewType == isOpenAQuestionView) {
             v = LayoutInflater.from(parent.context).inflate(R.layout.openaquestiondisplaynewlayout, parent, false)
             vh = OpenAQuestionDataHolder(v)
@@ -345,19 +323,7 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
 
                 openAQuestionHelperFunctions.OpenQuestionClickPasser(vhf)
             }
-
-        } else if (viewType == isNotificationView) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.fragment_notification_manager_view, parent, false)
-            vh = NotificationViewHolder(v)
-        } else if (viewType == isTenorView) {
-            v = LayoutInflater.from(parent.context).inflate(R.layout.imageandgifview, parent, false)
-            vh = TenorGifDataHolder(v)
-            val vhf = vh
-            v!!.setOnClickListener {
-                tenorHelperFunctions.OpenQuestionClickPasser(vhf)
-                //SetVisibilityOfButtons(VhUniv);
-            }
-        }*/
+        */
         return vh as RecyclerView.ViewHolder
     }
 
@@ -410,7 +376,6 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
 
     }
 
-
     fun clear() {
         mValues?.clear()
         notifyDataSetChanged()
@@ -433,10 +398,6 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
 
 
     override fun getItemViewType(position: Int): Int {
-        //return mValues.get(position).get;
-
-        /*if (position % 20 == 0)
-            return isadview;*/
 
         val ins = mValues?.get(position)
         if (ins is FeedArticleDataHolder.FeedArticleHolder) {
@@ -473,25 +434,6 @@ public class AllRecyclerViewAdapter(activity: Activity, items: MutableList<Any>,
         }
 
         return isFeedView
-        /*else if (ins is Long) {
-            return isadview
-        } else if (ins is DateTypeAndStringHolder) {
-            return isdateview
-        } else if (ins is ForReturningQuestionsLite) {
-            return isQuestionView
-        } else if (ins is PeopleDataHolder) {
-            return isPeopleView
-        } else if (ins is NothingToShowDataHolder) {
-            return isNoMessageView
-        } else if (ins is OpenAQuestionClassForShowingData) {
-            return isOpenAQuestionView
-        } else if (ins is QuestionNotifications) {
-            return isNotificationView
-        } else if (ins is JsonTenorTrendingMedium) {
-            return isTenorView
-        }
-
-        return if (position % 10 == 0) isadview else ischatview*/
     }
 
 
--- app/src/main/java/com/steemapp/lokisveil/steemapp/Interfaces/AllRecyclerViewAdapterInterface.kt
+++ app/src/main/java/com/steemapp/lokisveil/steemapp/Interfaces/AllRecyclerViewAdapterInterface.kt
@@ -6,8 +6,6 @@ import com.steemapp.lokisveil.steemapp.DataHolders.FeedArticleDataHolder
  * Created by boot on 2/4/2018.
  */
 interface AllRecyclerViewAdapterInterface {
-    abstract fun SetMenuItemVisibility(MenuItemId: Int, visibility: Boolean)
-    abstract fun SetData(data: FeedArticleDataHolder.FeedArticleHolder)
-   // fun GetFragmentManager()
-    //abstract fun SetData(data: JsonTenorTrendingMedium)
+    fun SetMenuItemVisibility(MenuItemId: Int, visibility: Boolean)
+    fun SetData(data: FeedArticleDataHolder.FeedArticleHolder)
 }
