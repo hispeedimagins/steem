6038f30435451b8a90ebe0699b531cccd8a7e755
hispeedimagins <36503820+hispeedimagins@users.noreply.github.com>
2018-12-16 16:36:43 +0530

    shared preferences singleton class to access it easily and cleanly from one place.

---
+++ app/src/main/java/com/steemapp/lokisveil/steemapp/HelperClasses/SharedPrefrencesSingleton.kt
@@ -0,0 +1,85 @@
+package com.steemapp.lokisveil.steemapp.HelperClasses
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.steemapp.lokisveil.steemapp.CentralConstants
+import java.lang.IllegalArgumentException
+
+class SharedPrefrencesSingleton {
+    companion object {
+
+        var sharedPreferences : SharedPreferences? = null
+
+        @Volatile
+        var sharedPreferencesSingleton : SharedPrefrencesSingleton? = null
+        var sharedPreferencesEdit: SharedPreferences.Editor? = null
+
+
+        fun getInstance(context: Context?, prefName:String = ""):SharedPrefrencesSingleton{
+            if(context == null && sharedPreferencesSingleton != null) return sharedPreferencesSingleton!!
+
+            if(sharedPreferencesSingleton != null && sharedPreferences != null){
+                return sharedPreferencesSingleton!!
+            } else if(sharedPreferencesSingleton == null){
+                synchronized(SharedPrefrencesSingleton::class.java){
+                    if(sharedPreferencesSingleton == null){
+                        sharedPreferencesSingleton = SharedPrefrencesSingleton()
+                        sharedPreferences = context?.getSharedPreferences(if(prefName.isNotBlank()) prefName else CentralConstants.sharedprefname, Context.MODE_PRIVATE)
+                    }
+                }
+                return sharedPreferencesSingleton!!
+            }
+            throw IllegalArgumentException("Must provide context atleast once")
+        }
+    }
+
+
+
+
+    fun edit(){
+        sharedPreferencesEdit = sharedPreferences!!.edit()
+    }
+
+    fun commit(){
+        sharedPreferencesEdit!!.commit()
+        sharedPreferencesEdit = null
+    }
+
+    fun put(key:String,value:Any,commit:Boolean = false){
+        if(sharedPreferencesEdit == null) edit()
+        when(value){
+            is String -> sharedPreferencesEdit!!.putString(key,value)
+            is Boolean -> sharedPreferencesEdit!!.putBoolean(key,value)
+            is Int -> sharedPreferencesEdit!!.putInt(key,value)
+            is Long -> sharedPreferencesEdit!!.putLong(key,value)
+            is Float -> sharedPreferencesEdit!!.putFloat(key,value)
+        }
+        if(commit) commit()
+    }
+
+    fun remove(key:String,commit:Boolean = false){
+        if(sharedPreferencesEdit == null) edit()
+        sharedPreferencesEdit?.remove(key)
+        if(commit) commit()
+    }
+
+    fun getString(key:String):String{
+        return sharedPreferences!!.getString(key,"")!!
+    }
+
+    fun getBoolean(key:String):Boolean{
+        return sharedPreferences!!.getBoolean(key,false)!!
+    }
+
+    fun getInt(key:String):Int{
+        return sharedPreferences!!.getInt(key,0)!!
+    }
+
+    fun getLong(key:String):Long{
+        return sharedPreferences!!.getLong(key,0L)!!
+    }
+
+    fun getFloat(key:String):Float{
+        return sharedPreferences!!.getFloat(key,0F)!!
+    }
+}
